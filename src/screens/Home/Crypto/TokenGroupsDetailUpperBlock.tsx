65b60f5a816d4dc21cc6533f764d00dacb765eeb
Buns Enchantress <68980965+BunsDev@users.noreply.github.com>
2023-10-28 23:43:46 -0500

    fix: CoinGecko chart URL

--- src/screens/Home/Crypto/TokenGroupsDetailUpperBlock.tsx
+++ src/screens/Home/Crypto/TokenGroupsDetailUpperBlock.tsx
@@ -1,28 +1,39 @@
-import React, { useMemo, useRef, useState } from 'react';
-import { Platform, StyleSheet, View } from 'react-native';
-import ActionButton from 'components/ActionButton';
-// import i18n from 'utils/i18n/i18n';
-import { CaretLeft } from 'phosphor-react-native';
-import { SwNumberProps } from 'components/Design/Number';
-import Number from 'components/Design/Number';
-import { BalancesVisibility } from 'components/BalancesVisibility';
-import { Button, Icon, Typography } from 'components/Design';
-import { useSoulWalletTheme } from 'hooks/useSoulWalletTheme';
-import { FontSemiBold } from 'styles/sharedStyles';
-// import { getAccountType } from 'utils/index';
-// import { PREDEFINED_TRANSAK_TOKEN } from '../../../predefined/transak';
-// import { RootState } from 'stores/index';
-// import { useSelector } from 'react-redux';
-import { ThemeTypes } from 'styles/themes';
-import { ButtonIcon } from 'screens/Home/Crypto/shared/Button';
-import { useNavigation } from '@react-navigation/native';
-import { RootNavigationProps } from 'routes/index';
-import { ColorMap } from 'styles/color';
-// import WebView from 'react-native-webview';
+import React, { useMemo, useRef, useState } from 'react'
+import { Platform, StyleSheet, View } from 'react-native'
+import ActionButton from 'components/ActionButton'
+// import i18n from 'utils/i18n/i18n'
+import { CaretLeft } from 'phosphor-react-native'
+import { SwNumberProps } from 'components/Design/Number'
+import Number from 'components/Design/Number'
+import { BalancesVisibility } from 'components/BalancesVisibility'
+import { Button, Icon, Typography } from 'components/Design'
+import { useSoulWalletTheme } from 'hooks/useSoulWalletTheme'
+import { FontSemiBold } from 'styles/sharedStyles'
+// import { getAccountType } from 'utils/index'
+// import { PREDEFINED_TRANSAK_TOKEN } from '../../../predefined/transak'
+// import { RootState } from 'stores/index'
+// import { useSelector } from 'react-redux'
+import { ThemeTypes } from 'styles/themes'
+import { ButtonIcon } from 'screens/Home/Crypto/shared/Button'
+import { useNavigation } from '@react-navigation/native'
+import { RootNavigationProps } from 'routes/index'
+import { ColorMap } from 'styles/color'
+import useTokenBalanceKeyPriceMap from 'hooks/screen/hooks/useTokenBalanceKeyPriceMap'
+import useTokenGroup from 'hooks/screen/hooks/useTokenGroup'
+import { useSelector } from 'react-redux'
+import useGetNetworkJson from 'hooks/screen/hooks/useGetNetworkJson'
+import useShowedNetworks from 'hooks/screen/hooks/useShowedNetworks'
+import { RootState } from 'stores/index'
+// import WebView from 'react-native-webview'
+import BigN from 'bignumber.js'
+import Text from 'components/Text'
+import { _getAssetPriceId } from '@soul-wallet/extension-base/src/services/chain-service/utils'
 
 interface Props {
   balanceValue: SwNumberProps['value'];
+  balanceUSD: SwNumberProps['value'];
   groupSymbol: string;
+  groupPriceId: string;
   tokenGroupSlug: string;
   onClickBack: () => void;
   onOpenSendFund?: () => void;
@@ -34,41 +45,21 @@ export const TokenGroupsDetailUpperBlock = ({
   onOpenSendFund,
   onClickBack,
   balanceValue,
+  balanceUSD,
   groupSymbol,
+  groupPriceId,
   tokenGroupSlug,
 }: Props) => {
   const navigation = useNavigation<RootNavigationProps>();
   const theme = useSoulWalletTheme().swThemes;
-  // const accounts = useSelector((state: RootState) => state.accountState.accounts);
-  // const currentAccount = useSelector((state: RootState) => state.accountState.currentAccount);
-  // const isAllAccount = useSelector((state: RootState) => state.accountState.isAllAccount);
-  const _style = createStyleSheet(theme);
-  // const isSupportBuyTokens = useMemo(() => {
-  //   if (Platform.OS === 'ios') {
-  //     return false;
-  //   }
-  //   const transakInfoItems = Object.values(PREDEFINED_TRANSAK_TOKEN);
-
-  //   for (const infoItem of transakInfoItems) {
-  //     if (infoItem.symbol === groupSymbol) {
-  //       const supportType = infoItem.support;
-
-  //       if (isAllAccount) {
-  //         for (const account of accounts) {
-  //           if (supportType === getAccountType(account.address)) {
-  //             return true;
-  //           }
-  //         }
-  //       } else {
-  //         if (currentAccount?.address && supportType === getAccountType(currentAccount?.address)) {
-  //           return true;
-  //         }
-  //       }
-  //     }
-  //   }
-
-  //   return false;
-  // }, [accounts, currentAccount?.address, isAllAccount, ]);
+  console.log('tokenSlug: %s', tokenGroupSlug);
+  const isSoul =
+    tokenGroupSlug == 'custom-fantom-ERC20-SOUL-0xe2fb177009FF39F52C0134E8007FA0e4BaAcBd07' ||
+    tokenGroupSlug == 'custom-custom-EVM-avalanchec-chain-43114-ERC20-SOUL-0x11d6DD25c1695764e64F439E32cc7746f3945543'
+  const _style = createStyleSheet(theme)
+  const balance = new BigN(balanceValue)
+  const tokenPrice = isSoul ? new BigN(0.0012) : new BigN(balanceUSD).div(balance)
+  const USDValue = isSoul ? tokenPrice.times(balance) : balanceUSD
 
   return (
     <View style={_style.containerStyle} pointerEvents="box-none">
@@ -86,11 +77,22 @@ export const TokenGroupsDetailUpperBlock = ({
           </Typography.Title>
         </View>
       </View>
-    {/* UI NOTE: Balance Summary (token detail view) */}
-      <BalancesVisibility value={balanceValue} startWithSymbol subFloatNumber />
+      {/* UI NOTE: Balance Summary (token detail view) */}
+      <BalancesVisibility value={USDValue} startWithSymbol subFloatNumber />
+      {/* <Number value={balanceValue} subFloatNumber decimal={0} /> */}
+      <Text
+        style={{
+          color: theme.colorTextLight1,
+          fontSize: 12,
+          marginTop: 4,
+          ...FontSemiBold,
+        }}
+      >
+        {`${balance.toFixed(4)} ${groupSymbol}`}
+      </Text>
 
       <View style={[_style.actionButtonWrapper]} pointerEvents="box-none">
-      <ActionButton
+        <ActionButton
           // label={i18n.cryptoScreen.send}
           // image={'https://raw.githubusercontent.com/SoulSwapFinance/assets/master/mobile/icons/sent.png'}
           // imageSize={30}
@@ -107,30 +109,30 @@ export const TokenGroupsDetailUpperBlock = ({
           buttonWrapperStyle={{ borderRadius: 32, paddingHorizontal: 1, paddingVertical: 1, backgroundColor: ColorMap.backgroundSecondary, marginLeft: 12, marginRight: 12 }}
         />
         {/* {Platform.OS !== 'ios' && ( */}
-          <ActionButton
-            // label={i18n.cryptoScreen.buy}
-            // image={'https://raw.githubusercontent.com/SoulSwapFinance/assets/master/mobile/icons/purchased.png'}
-            // imageSize={30}  
-            icon={ButtonIcon.Buy}
-            onPress={() => navigation.navigate('Drawer', { screen: 'BuyToken', params: {} })}
-            buttonWrapperStyle={{ borderRadius: 32, paddingHorizontal: 1, paddingVertical: 1, backgroundColor: ColorMap.backgroundSecondary, marginLeft: 12, marginRight: 12 }}
-          />
-      <ActionButton
-            // label={'Swap'}
-            // image={'https://raw.githubusercontent.com/SoulSwapFinance/assets/master/mobile/icons/purchased.png'}
-            // imageSize={30}  
-            icon={ButtonIcon.Charts}
-            // UI NOTE: OPEN URL IN BROWSER
-            // onPress={toggleTokenView}
-            onPress={ () => navigation.navigate('BrowserTabsManager', { url: `https://coingecko.com/en/coins/${'soul-swap'}?utm_source=soulswap`, name: 'SoulSwap' }) }
-            // @ts-ignore
-            // onPress={() => navigation.navigate('Home', { screen: 'Main', params: {screen: "NFTs", params: {} }})}
-            // params: {
-              //   // @ts-ignore
-              //   screen: 'NFTs'})}
-              buttonWrapperStyle={{ borderRadius: 32, paddingHorizontal: 1, paddingVertical: 1, backgroundColor: ColorMap.backgroundSecondary, marginLeft: 12, marginRight: 12 }}
-              />
-        </View>
+        <ActionButton
+          // label={i18n.cryptoScreen.buy}
+          // image={'https://raw.githubusercontent.com/SoulSwapFinance/assets/master/mobile/icons/purchased.png'}
+          // imageSize={30}  
+          icon={ButtonIcon.Buy}
+          onPress={() => navigation.navigate('Drawer', { screen: 'BuyToken', params: {} })}
+          buttonWrapperStyle={{ borderRadius: 32, paddingHorizontal: 1, paddingVertical: 1, backgroundColor: ColorMap.backgroundSecondary, marginLeft: 12, marginRight: 12 }}
+        />
+        <ActionButton
+          // label={'Charts'}
+          // image={'https://raw.githubusercontent.com/SoulSwapFinance/assets/master/mobile/icons/purchased.png'}
+          // imageSize={30}  
+          icon={ButtonIcon.Charts}
+          // UI NOTE: OPEN URL IN BROWSER
+          // onPress={toggleTokenView}
+          onPress={() => navigation.navigate('BrowserTabsManager', { url: `https://coingecko.com/en/coins/${groupPriceId ?? 'soul-swap'}?utm_source=soulswap`, name: 'SoulSwap' })}
+          // @ts-ignore
+          // onPress={() => navigation.navigate('Home', { screen: 'Main', params: {screen: "NFTs", params: {} }})}
+          // params: {
+          //   // @ts-ignore
+          //   screen: 'NFTs'})}
+          buttonWrapperStyle={{ borderRadius: 32, paddingHorizontal: 1, paddingVertical: 1, backgroundColor: ColorMap.backgroundSecondary, marginLeft: 12, marginRight: 12 }}
+        />
+      </View>
     </View>
 
   );
